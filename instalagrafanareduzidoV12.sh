3a25ebc207ffa1fc94c817d278b4a21120e509f0
José Gabriel dos Santos Silva <150761763+JG7s@users.noreply.github.com>
2025-06-27 23:53:03 -0300

    Create instalagrafanareduzidoV12.sh

---
+++ instalagrafanareduzidoV12.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+# Define o IP do servidor Zabbix #
+IP_SERVIDOR="172.17.1.6"
+
+
+echo "Atualizando Linux"
+apt update -y
+apt upgrade -y
+apt update -y
+
+
+echo "Instalando Grafava V12"
+apt-get install -y adduser libfontconfig1 musl
+
+#Instala o Grafana v12
+wget https://dl.grafana.com/oss/release/grafana_12.0.0_amd64.deb
+dpkg -i grafana_12.0.0_amd64.deb
+
+apt update
+
+#Baixar Plugins 
+grafana-cli plugins install alexanderzobnin-zabbix-app
+
+grafana-cli plugins install volkovlabs-echarts-panel
+
+grafana-cli plugins install grafana-clock-panel
+
+grafana-cli plugins update-all
+
+#Inicializa o Grafana junto com a Vm
+systemctl daemon-reload
+systemctl enable --now grafana-server
+systemctl start grafana-server
